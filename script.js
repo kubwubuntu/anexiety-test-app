4d19256cc35cedd9bbc0e124d0f7376d274e63a3
NIYIBIZI HIRWA <hirwadeveloper@gmail.com>
2022-02-03 12:03:03 +0200

    uio

--- script.js
+++ script.js
@@ -5,16 +5,16 @@ formElement.onsubmit = function submitAnswers(){
 	var score = 0;
 	
 	// Get User Input
-	var q1 = document.forms["quizForm"]["q1"].value,
-	    q2 = document.forms["quizForm"]["q2"].value,
-	    q3 = document.forms["quizForm"]["q3"].value,
-	    q4 = document.forms["quizForm"]["q4"].value,
-	    q5 = document.forms["quizForm"]["q5"].value;
-		q6 = document.forms["quizForm"]["q6"].value;
-		q7 = document.forms["quizForm"]["q7"].value;
+	var q1 = parseInt(document.forms["quizForm"]["q1"].value),
+	    q2 = parseInt(document.forms["quizForm"]["q2"].value),
+	    q3 = parseInt(document.forms["quizForm"]["q3"].value),
+	    q4 = parseInt(document.forms["quizForm"]["q4"].value),
+	    q5 = parseInt(document.forms["quizForm"]["q5"].value),
+		q6 = parseInt(document.forms["quizForm"]["q6"].value),
+		q7 = parseInt(document.forms["quizForm"]["q7"].value);
 	
 	// Check Answers
-	var result = q1 + q2 + q3 + q4 + q5 + q6 + q7 * 1 ;
+	var result = (q1 + q2 + q3 + q4 + q5 + q6 + q7 ) * 1 ;
 	
 	// Display Results
 	var results = document.getElementById('results');
