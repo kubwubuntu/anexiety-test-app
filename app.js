f002604b6a9ffbc6a520b1ca585145c459fc7312
NIYIBIZI HIRWA <hirwadeveloper@gmail.com>
2022-02-23 14:54:13 +0200

    iop

--- app.js
+++ app.js
@@ -55,6 +55,7 @@ function nextPrev(n) {
       q16 = parseInt(document.forms["quizForm"]["q16"].value),
       q17 = parseInt(document.forms["quizForm"]["q17"].value),
       q18 = parseInt(document.forms["quizForm"]["q18"].value);
+      q19 = parseInt(document.forms["quizForm"]["q19"].value);
 	
 	// Check Answers
 	var result = (q1 + q2 + q3 + q4 + q5 + q6 + q7 + q8 + q9 + q10 + q11 + q12 + q13 + q14  + q15 + q16 + q17 + q18 + q19 + q20  + q21) * 1 ;
