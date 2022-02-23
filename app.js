1a2acb350c548afc8ba6a30ed293c27e3dad885e
NIYIBIZI HIRWA <hirwadeveloper@gmail.com>
2022-02-23 14:49:36 +0200

    uio

--- app.js
+++ app.js
@@ -154,7 +154,7 @@ formElement.onsubmit = function submitAnswers(){
         q21 = parseInt(document.forms["quizForm"]["q21"].value);
 	
 	// Check Answers
-	var result = (q1 + q2 + q3 + q4 + q5 + q6 + q7 + q8 + q9 + q10 + Q11 + q12 + q13 + q14  + q15 + q16 + q17 + q18 ) * 1 ;
+	var result = (q1 + q2 + q3 + q4 + q5 + q6 + q7 + q8 + q9 + q10 + Q11 + q12 + q13 + q14  + q15 + q16 + q17 + q18 + q19 + q20 + q21 ) * 1 ;
 	
 	// Display Results
 	var results = document.getElementById('results');
